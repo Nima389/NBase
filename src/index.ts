cc9ad4bfe0125ca126ad708cd48ddd1b1c7be54d
Nima <zamaninima1234@gmail.com>
2026-09-11 23:07:32 +0330

    First commit!

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const app = express();
+const port = process.env.PORT;
+
+app.init();
+
+app.get("/NBase", (_req, res) => {
+  res.status(200).sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, function () {
+  console.log(`SERVER STARTED ON ${port}`);
+});
